8b42d198967c69ff0f295e52d61461843d0cec7b
MircoH0 <46491467+MircoH0@users.noreply.github.com>
2023-09-08 17:23:35 +0800

    Update rocm-fan-control.sh

--- rocm-fan-control.sh
+++ rocm-fan-control.sh
@@ -3,7 +3,7 @@
 trap "echo -e \"\nPress X key to exit\"" 2
 echo -e "\033[?25l"
 
-refresh_time=3
+refresh_interval=3
 smartfan=0
 sf_autotemp=55
 sf_inauto=0
@@ -46,7 +46,7 @@ do
 		sf_info=$sf_info"OFF"
 	fi
 	
-	sf_info=$sf_info"\nTrigger temperature : ${sf_autotemp}c\nRefresh interval : ${refresh_time}s"
+	sf_info=$sf_info"\nTrigger temperature : ${sf_autotemp}c\nRefresh interval : ${refresh_interval}s"
 	
 	clear
 	rocm-smi | sed -n 4,7p
@@ -56,7 +56,7 @@ do
 	echo -e "\n________________Set GPU fan speed________________\n\n   0-9 : 0%-90%\n   +/- : Change smart fan trigger temperature\n   [/] : Change info refresh interval\n   F : 100%\n   A : Auto control by display driver\n   S : Switch smart fan mode (ON/OFF)\n   Q : Set to driver control and exit\n   X : Exit\n"
 	echo -e $sf_info
 	
-	if read -s -t $refresh_time -n1 userinput
+	if read -s -t $refresh_interval -n1 userinput
 	then
 		echo
 		case $userinput in
